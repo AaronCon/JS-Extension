4e30d14a40880d0cb14241c989381c30056c742b
AaronCon <2905529185@qq.com>
2022-12-01 18:43:48 +0800

    80_TodoList1_over

--- 80_TodoList/js/index.js
+++ 80_TodoList/js/index.js
@@ -10,29 +10,107 @@ var initTodoList = (function() {
       addItem = document.getElementsByClassName('j-add-item')[0],
       textInput = document.getElementById('textInput'),
       oList = document.getElementsByClassName('j-list')[0],
-      inputShow = false;
+      inputShow = false,
+      isEdit = false,
+      curIdx = null;
 
   addEvent(showInput, 'click', function() {
     if(!inputShow) {
-      inputWrap.style.display = "block";
-      inputShow = true;
+      inputSwitch('open');
     } else {
-      inputWrap.style.display = "none";
-      inputShow = false;
+      inputSwitch('close');
+      restStaus();
     }
   })
 
   addEvent(addItem, 'click', function() {
     var val = textInput.value,
-        len = val.length;
+        len = val.length,
+        oItems = document.getElementsByClassName('item'),
+        itemLen = oItems.length;
 
     if(len === 0) {
       return;
     }
 
-    var oli = document.createElement('li');
-    oli.className = 'item';
-    oli.innerText = val;
-    oList.appendChild(oli);
+    if(itemLen > 0) {
+      for(var i = 0; i < itemLen; i++) {
+        itemText = elemChildren(oItems[i])[0].innerText;
+
+        if(itemText === val) {
+          alert('已存在该项目');
+          return;
+        }
+      }
+    }
+
+    if(isEdit) {
+      var oitem = elemChildren(oItems[curIdx])[0];
+      oitem.innerHTML = val;
+      oItems[curIdx].className = 'item';
+    } else {
+      var oli = document.createElement('li');
+      oli.className = 'item';
+      oli.innerHTML = itemTpl(textInput.value);
+      oList.appendChild(oli);
+    }
+    restStaus();
   })
+
+  addEvent(oList, 'click', function(e) {
+    var e = e || window.event,
+        tar = e.target || e.srcElement,
+        className = tar.className,
+        oItems = document.getElementsByClassName('item'),
+        item,
+        oParent = elemParent(tar, 2),
+        tarIdx = Array.prototype.indexOf.call(oItems, oParent);
+
+    if(className === 'edit-btn fa fa-edit') {
+      var itemLen = oItems.length;
+
+      textInput.value = oParent.getElementsByTagName('p')[0].innerHTML;
+      inputSwitch('open');
+
+      for(var i = 0; i < itemLen; i++) {
+        item = oItems[i];
+        item.className = ' item';
+      }
+      curIdx = tarIdx;
+      oParent.className += ' active';
+      addItem.innerText = '编辑第'+ (curIdx + 1) + '项';
+      isEdit = true;
+    } else if(className === 'remove-btn fa fa-times') {
+      restStaus();
+      oParent.remove();
+    }
+  })
+
+  function restStaus() {
+    isEdit = false;
+    curIdx = null;
+    textInput.value = '';
+    addItem.innerText = '增加项目';
+    inputSwitch('close');
+  }
+
+  function inputSwitch(status) {
+    if(status === 'open') {
+      inputWrap.style.display = "block";
+      inputShow = true;
+    } else if (status === 'close') {
+      inputWrap.style.display = "none";
+      inputShow = false;
+    }
+  }
+
+  function itemTpl(text) {
+    return (
+      '<p class="item-content">' + text + '</p>' +
+      '<div class="btn-group">'+
+        '<a href="javascript:;" class="edit-btn fa fa-edit"></a>'+
+        '<a href="javascript:;" class="remove-btn fa fa-times"></a>'+
+      '</div>'
+    )
+  }
 })();
--- 80_TodoList/js/utils.js
+++ 80_TodoList/js/utils.js
@@ -9,3 +9,36 @@ function addEvent(el, type, fn) {
     el['on' + type] = fn;
   }
 }
+
+function elemParent(node, n) {
+  var type = typeof(n);
+  if(type === 'undefined') {
+    return node.parentNode;
+  } else if(n <= 0 || type !== 'number') {
+    return undefined;
+  }
+
+  while(n) {
+    node = node.parentNode;
+    n--;
+  }
+  return node;
+}
+
+function elemChildren(node) {
+  var temp = {
+    'length': 0,
+    'splice': Array.prototype.splice
+  },
+  len = node.childNodes.length;
+
+  for(var i = 0; i < len; i++) {
+    var childItem = node.childNodes[i];
+
+    if(childItem.nodeType === 1) {
+      temp[temp.length] = childItem;
+      temp['length']++;
+    }
+  }
+  return temp;
+}
